6cb9133f8d2b722bc3c76676c0753a4cc924ec82
Albert Toledo <toledoalbert@gmail.com>
2015-03-15 19:13:40 -0400

    initial commit for dice web client, login section and navbar markup done

---
+++ app/controllers/main.js
@@ -0,0 +1,8 @@
+angular.module('app')
+
+
+.controller('MainController', ['$scope', '$state', function($scope, $state) {
+
+    $scope.loggedIn = false;
+
+}]);
---
+++ app/directives/article-item/article-item.js
@@ -0,0 +1,49 @@
+angular.module('hunews.directives.articleItem', [])
+	.directive('articleItem', function() {
+		return {
+			restrict: 'E',
+			scope: {
+				article: '=',
+				loadArticles: '=',
+				editArticle: '='
+			},
+			templateUrl: 'app/directives/article-item/article-item.html',
+			controller: function($scope, $element) {
+				$scope.editThis = $scope.editArticle;
+				$scope.title = $scope.article.title;
+				$scope.visible = $scope.article.visible;
+				$scope.img = $scope.article.image;
+
+				console.log($element);
+				$element.find('li').css('background', 'url(' + $scope.img + ') no-repeat');
+
+				$scope.showHideArticle = function(value) {
+
+					var query = new Parse.Query("Article");
+
+			        query.get($scope.article.id, {
+
+			          success: function(article) {
+
+			            // The object was retrieved successfully.
+			            article.set("visible", value);
+			            article.save();
+			            article.save(null, {
+
+			              success: function(art) {
+			              	$scope.loadArticles();
+			              }
+
+			            });
+
+			          },
+			          error: function(object, error) {
+			            // The object was not retrieved successfully.
+			            // error is a Parse.Error with an error code and message.
+			            console.log("Error occured: " + error.message);
+			          }
+			        });
+				};
+			}
+		}
+	});
